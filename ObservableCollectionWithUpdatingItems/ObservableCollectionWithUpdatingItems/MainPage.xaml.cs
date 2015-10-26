96437739f4bcab7bbcbb366d200d0e412b88628e
Andrew Bares <anbare@microsoft.com>
2015-10-26 08:37:34 -0700

    Created sample

---
+++ ObservableCollectionWithUpdatingItems/ObservableCollectionWithUpdatingItems/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ObservableCollectionWithUpdatingItems
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<ToDoItem> _toDoItems = new ObservableCollection<ToDoItem>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            listView.ItemsSource = _toDoItems;
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            _toDoItems.Add(new ToDoItem()
+            {
+                Name = "Item " + (_toDoItems.Count + 1),
+                Date = DateTime.Now
+            });
+        }
+
+        private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            if (_toDoItems.Count == 0)
+                return;
+
+            int randomIndex = new Random().Next(0, _toDoItems.Count);
+
+            ToDoItem item = _toDoItems[randomIndex];
+
+            item.Name = "Updated " + item.Name;
+            item.Date = DateTime.Now;
+        }
+    }
+}
---
+++ ObservableCollectionWithUpdatingItems/ObservableCollectionWithUpdatingItems/ToDoItem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableCollectionWithUpdatingItems
+{
+    public class ToDoItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+
+        private DateTime _date;
+        public DateTime Date
+        {
+            get { return _date; }
+            set { SetProperty(ref _date, value); }
+        }
+
+        /// <summary>
+        /// Checks if a property already matches a desired value.  Sets the property and
+        /// notifies listeners only when necessary.
+        /// </summary>
+        /// <typeparam name="T">Type of the property.</typeparam>
+        /// <param name="storage">Reference to a property with both getter and setter.</param>
+        /// <param name="value">Desired value for the property.</param>
+        /// <param name="propertyNames">Name of the property used to notify listeners.  This
+        /// value is optional and can be provided automatically when invoked from compilers that
+        /// support CallerMemberName.</param>
+        /// <returns>True if the value was changed, false if the existing value matched the
+        /// desired value.</returns>
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (object.Equals(storage, value)) return false;
+
+            storage = value;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        /// <summary>
+        /// Notifies listeners that a property value has changed.
+        /// </summary>
+        /// <param name="propertyNames">Name of the property used to notify listeners.</param>
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var eventHandler = this.PropertyChanged;
+            if (eventHandler != null)
+            {
+                eventHandler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
